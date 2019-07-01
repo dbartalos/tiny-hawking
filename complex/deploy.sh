611cf47eff0edfa2e50941600470e8ec5082d7a0
David Bartalos <6267612+dbartalos@users.noreply.github.com>
2019-07-01 23:10:58 +0100

    added deployment scripts

---
+++ complex/deploy.sh
@@ -0,0 +1,16 @@
+docker build -t dbartalos/multi-client:latest -t dbartalos/multi-client:$SHA -f ./complex/client/Dockerfile ./client
+docker build -t dbartalos/multi-server:latest -t dbartalos/multi-server:$SHA -f ./complex/server/Dockerfile ./server
+docker build -t dbartalos/multi-worker:latest -t dbartalos/multi-worker:$SHA -f ./complex/worker/Dockerfile ./worker
+
+docker push dbartalos/multi-client:latest
+docker push dbartalos/multi-server:latest
+docker push dbartalos/multi-worker:latest
+
+docker push dbartalos/multi-client:$SHA
+docker push dbartalos/multi-server:$SHA
+docker push dbartalos/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=dbartalos/multi-server:$SHA
+kubectl set image deployments/client-deployment client=dbartalos/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=dbartalos/multi-worker:$SHA
